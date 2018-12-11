d3e5fdc84c3b36e920a826f7c8eed4f0f02e9dca
Garrett Wilson <g@floft.net>
2018-12-11 14:07:29 -0800

    Stream via RSTP for QGroundControl app, use MAVProxy

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ echo "--"
 echo "Don't forget to enable the hotspot!"
 echo "--"
 ./object_detector.py --remote --debug --record=record --gst
+#./object_detector.py --remote --debug --record=record --gst --host=10.42.0.183
